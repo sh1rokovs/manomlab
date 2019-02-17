1925d71bf99ec71c0e45bce78f78706f0b401fd0
Sergey <hilallod@yandex.ru>
2019-02-17 15:32:51 +0300

    ddd

---
+++ Project10/Tlist.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+//asd
+class TLink
+{
+public:
+	T val;
+	TLink *pNext;
+};
+
+template <class T>
+class THeadList
+{
+protected:
+	TLink<T> *pHead, *pFirst, *pPrev, *pCurr, *pLast;
+	int pos, size;
+public:
+
+	THeadList()
+	{
+		TLink<T>*tmp = new TLink<T>;
+		pHead = tmp;
+		pHead->pNext = pHead;
+		pFirst = pCurr = pLast = pPrev = NULL;
+		pos = -1;
+		size = 0;
+	}
+
+
+	void InsFirst(const T elem)
+	{
+		TLink<T>*tmp = new TLink<T>;
+		tmp->val = elem;
+		if (size > 0)
+		{
+			pHead->pNext = tmp;
+			tmp->pNext = pFirst;
+			pFirst = tmp;
+			size++; pos++;
+		}
+		else
+		{
+			pHead->pNext = tmp;
+			tmp->pNext = pHead;
+			pFirst = tmp;
+			size++; pos = 0;
+			pCurr = pFirst;
+		}
+	}
+
+	void InsCurr(const T elem)
+	{
+		TLink<T>*tmp = new TLink<T>;
+		tmp->val = elem;
+		pPrev->pNext = tmp;
+		tmp->pNext = pCurr;
+		pCurr = tmp;
+		size++;
+	}
+
+	void DelCurr()
+	{
+		if (pCurr != pHead && size > 0)
+		{
+			TLink<T>*tmp = pCurr;
+			pPrev->pNext = pCurr->pNext;
+			delete tmp;
+			pCurr = pPrev->pNext;
+			size--;
+		}
+	}
+
+
+};
